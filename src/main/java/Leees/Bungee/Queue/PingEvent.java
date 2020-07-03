fdc3b50992721f8381c973552dc5593b4ecdea67
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-07-03 17:55:44 +0200

    Fix code for 1.16

--- src/main/java/Leees/Bungee/Queue/PingEvent.java
+++ src/main/java/Leees/Bungee/Queue/PingEvent.java
@@ -1,29 +1,39 @@
 package Leees.Bungee.Queue;
 
-import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
-import net.md_5.bungee.api.plugin.Plugin;
-import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.event.EventHandler;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 
 public class PingEvent implements Listener {
     @EventHandler
     public void onPing(ProxyPingEvent event) {
         if (Lang.SERVERPINGINFOENABLE.equals("true")) {
-            ServerPing.PlayerInfo one = new ServerPing.PlayerInfo((Lang.SERVERPINGINFO.replace("&", "§").replace("%priority%", "" + QueuePlugin.priorityqueue.size()).replace("%regular%", "" + QueuePlugin.regularqueue.size())), "1");
-            ServerPing.PlayerInfo[] info = {one};
+            ServerPing.PlayerInfo[] info = {};
+            int i = 0;
+
+            for (String str : Lang.SERVERPINGINFO) {
+                info = addInfo(info, new ServerPing.PlayerInfo(str.replaceAll("&", "§").replaceAll("%priority%", "" + QueuePlugin.priorityqueue.size()).replaceAll("%regular%", "" + QueuePlugin.regularqueue.size()), String.valueOf(i)));
+                i++;
+            }
+
             ServerPing.Players players = new ServerPing.Players(Lang.QUEUESERVERSLOTS, QueuePlugin.getInstance().getProxy().getOnlineCount(), info);
 
             ServerPing ping = new ServerPing(event.getResponse().getVersion(), players, event.getResponse().getDescriptionComponent(), event.getResponse().getFaviconObject());
             event.setResponse(ping);
         }
+    }
+
+    public static ServerPing.PlayerInfo[] addInfo(ServerPing.PlayerInfo[] arr, ServerPing.PlayerInfo info) {
+        int i;
+
+        ServerPing.PlayerInfo[] newarr = new ServerPing.PlayerInfo[arr.length + 1];
+
+        for (i = 0; i < arr.length; i++)
+            newarr[i] = arr[i];
+
+        newarr[arr.length] = info;
+
+        return newarr;
     }
 }
