d044cc75d31ddaa29169cbbe54941a3b19d4b5ba
Leeeunderscore <43287584+Leeeunderscore@users.noreply.github.com>
2021-05-12 14:23:31 -0600

    4.1.3
    added motds built into leeesbungeequeue enabled by default

--- src/main/java/Leees/Bungee/Queue/Bungee/PingEvent.java
+++ src/main/java/Leees/Bungee/Queue/Bungee/PingEvent.java
@@ -1,15 +1,38 @@
 package Leees.Bungee.Queue.Bungee;
 
 import net.md_5.bungee.api.ServerPing;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+import Leees.Bungee.Queue.Bungee.Lang;
 import net.md_5.bungee.event.EventPriority;
+
+import java.util.Random;
 
 public class PingEvent implements Listener {
     ServerPing.Protocol protocol;
+    private final Random random = new Random();
     @EventHandler(priority = EventPriority.HIGH)
     public void onPing(ProxyPingEvent event) {
+        int total;
+
+        total = LeeesBungeeQueue.priorityqueue.size() + LeeesBungeeQueue.regularqueue.size();
+      if (Lang.ENABLEMOTD.contains("true")) {
+          if (!Lang.ENABLERANDOMMOTD.contains("true")) {
+              ServerPing serverPing = event.getResponse();
+              serverPing.setDescription(Lang.MOTD.replaceAll("&", "§").replaceAll("%priosize%", "" +
+                      LeeesBungeeQueue.priorityqueue.size()).replaceAll("%regsize%", "" + LeeesBungeeQueue.regularqueue.size())
+                      .replaceAll("%maxplayers%", "" + Lang.MAINSERVERSLOTS).replaceAll("%total%", "" + total));
+              event.setResponse(serverPing);
+          } else {
+              ServerPing serverPing = event.getResponse();
+              serverPing.setDescriptionComponent(new TextComponent(Lang.RANDOMMOTDS.get(random.nextInt(Lang.RANDOMMOTDS.size())).replaceAll("&", "§").replaceAll("%priosize%", "" +
+                      LeeesBungeeQueue.priorityqueue.size()).replaceAll("%regsize%", "" + LeeesBungeeQueue.regularqueue.size())
+                      .replaceAll("%maxplayers%", "" + Lang.MAINSERVERSLOTS).replaceAll("%total%", "" + total)));
+              event.setResponse(serverPing);
+          }
+        }
         if (Lang.SERVERPINGINFOENABLE.equals("true")) {
             if (!Lang.CUSTOMPROTOCOL.contains("false")) {
                 ServerPing.Protocol provided = event.getResponse().getVersion();
@@ -23,10 +46,6 @@ public class PingEvent implements Listener {
             }
             ServerPing.PlayerInfo[] info = {};
             int i = 0;
-
-            int total;
-
-            total = LeeesBungeeQueue.priorityqueue.size() + LeeesBungeeQueue.regularqueue.size();
 
             for (String str : Lang.SERVERPINGINFO) {
                 info = addInfo(info, new ServerPing.PlayerInfo(str.replaceAll("&", "§").replaceAll("%priority%", "" + LeeesBungeeQueue.priorityqueue.size()).replaceAll("%regular%", "" + LeeesBungeeQueue.regularqueue.size()).replaceAll("%maxplayers%", String.valueOf(Lang.MAINSERVERSLOTS)).replaceAll("%totalinqueue%", String.valueOf(total)), String.valueOf(i)));
